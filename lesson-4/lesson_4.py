3df0b49cacd0e72694810f2614690c73e0c85ec6
Andrey_Serokhvostov <sebkd@mail.ru>
2022-03-14 21:26:14 +0300

    add db and End L4

--- lesson-4/lesson_4.py
+++ lesson-4/lesson_4.py
@@ -1,7 +1,26 @@
+import hashlib
 import re
 from pprint import pprint
 import requests
 from lxml import html
+from pymongo import MongoClient
+from pymongo.errors import DuplicateKeyError
+
+
+def insert_db(dictionary, db_base, hash_link):
+    '''Функция по заполнению базы данных'''
+    for dict_v in dictionary.values():
+        try:
+            db_base.insert_one({
+                # _id заполняется посредством hash из ссылки, которая уникальна
+                # проверку hash делаем именно на этом этапе, так как база данных по идеи будет дополняться
+                # и делать проверку на более раннем этапе нецелесообразно, так как там просто делается словарь объектов
+                '_id': (hashlib.sha256(dict_v[hash_link].encode())).hexdigest(),
+                '_data': dict_v,
+            })
+        except DuplicateKeyError:
+            print('есть такой элемент')
+            continue
 
 
 def parsing_mailnews(container, news_xpath, link_source_xpath, item_list):
@@ -29,60 +48,71 @@ def parsing_mailnews(container, news_xpath, link_source_xpath, item_list):
             except IndexError:
                 item_dict['news'] = ''
 
-            items_list.append(item_dict.copy())
+            item_list[index] = item_dict.copy()
 
     return item_list
 
-# url = 'https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313&_nkw=hover+h3&_sacat=0'
-# url = 'https://ru.ebay.com/b/Fishing-Equipment-Supplies/1492/bn_1851047'
-url_news = 'https://news.mail.ru/'
-'''
-Для парсинга использовать xpath. Структура данных должна
-содержать:
-• название источника, source
-• наименование новости, news
-• ссылку на новость, link_news
-• дата публикации date_news
-'''
-headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
-                         'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 '
-                         'Safari/537.36'}
 
-response = requests.get(url_news, headers=headers)
-dom = html.fromstring(response.text)
+if __name__ == "__main__":
+    url_news = 'https://news.mail.ru/'
+    '''
+    Для парсинга использовать xpath. Структура данных должна
+    содержать:
+    • название источника, source
+    • наименование новости, news
+    • ссылку на новость, link_news
+    • дата публикации date_news
+    '''
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
+                             'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 '
+                             'Safari/537.36'}
 
-items_container_top = dom.xpath("//td[contains(@class, 'daynews__main')] | //td[contains(@class, 'daynews__items')]")
-items_container_ul = dom.xpath("//ul[contains(@class, 'list_half')]")
+    response = requests.get(url_news, headers=headers)
+    dom = html.fromstring(response.text)
 
-items_list = []
-XPATH_NEWS_TOP = ".//span[contains(@class, 'photo__title')]/text()"
-XPATH_LINK_SOURCE_TOP = ".//../../@href"
-XPATH_NEWS_UL = ".//li[@class='list__item']/a/text()"
-XPATH_LINK_SOURCE_UL = ".//li[@class='list__item']/a/@href"
+    items_container_top = dom.xpath(
+        "//td[contains(@class, 'daynews__main')] | //td[contains(@class, 'daynews__items')]")
+    items_container_ul = dom.xpath("//ul[contains(@class, 'list_half')]")
 
-items_list = parsing_mailnews(items_container_top, XPATH_NEWS_TOP, XPATH_LINK_SOURCE_TOP, items_list)
-items_list = parsing_mailnews(items_container_ul, XPATH_NEWS_UL, XPATH_LINK_SOURCE_UL, items_list)
+    # items_dict = []
+    items_dict = {}
+    XPATH_NEWS_TOP = ".//span[contains(@class, 'photo__title')]/text()"
+    XPATH_LINK_SOURCE_TOP = ".//../../@href"
+    XPATH_NEWS_UL = ".//li[@class='list__item']/a/text()"
+    XPATH_LINK_SOURCE_UL = ".//li[@class='list__item']/a/@href"
 
-pprint(items_list)
-# href = item.xpath(".//h3[@class='s-item__title']/../@href")[0]
-#     prices = item.xpath(".//span[@class='s-item__price']//text()")
-#     for index, price in enumerate(prices):
-#         if ' до ' in price:
-#             prices[index] = str(prices[index - 1]) + str(prices[index]) + str(prices[index + 1]).replace('\xa0', '')
-#             prices.pop(index + 1)
-#             prices.pop(index - 1)
-#         else:
-#             prices[index] = str(prices[index]).replace('\xa0', '')
-#         index += 1
-#     info = item.xpath(".//span[contains(@class, 's-item__hotness')]/span/text()")
-#     item_dict['name'] = names
-#     item_dict['href'] = href
-#     item_dict['price'] = prices[0]
-#     try:
-#         item_dict['info'] = info[0].replace('\xa0', '')
-#     except IndexError:
-#         item_dict['info'] = ''
-#
-#     items_list.append(item_dict)
-#
-# pprint(items_list)
+    items_dict = parsing_mailnews(items_container_top, XPATH_NEWS_TOP, XPATH_LINK_SOURCE_TOP, items_dict)
+    items_dict = parsing_mailnews(items_container_ul, XPATH_NEWS_UL, XPATH_LINK_SOURCE_UL, items_dict)
+
+    client = MongoClient('localhost', 27017)
+
+    db_mongo = client['mailnews']
+    db_mongo.drop_collection('mailnews')  # для полноценного стирания базы данных
+    mailnews = db_mongo.mailnews
+    hash_link = 'link_news'
+
+    insert_db(items_dict, mailnews, hash_link)
+    pprint(mailnews.find({}))
+    # pprint(items_dict)
+    # href = item.xpath(".//h3[@class='s-item__title']/../@href")[0]
+    #     prices = item.xpath(".//span[@class='s-item__price']//text()")
+    #     for index, price in enumerate(prices):
+    #         if ' до ' in price:
+    #             prices[index] = str(prices[index - 1]) + str(prices[index]) + str(prices[index + 1]).replace('\xa0', '')
+    #             prices.pop(index + 1)
+    #             prices.pop(index - 1)
+    #         else:
+    #             prices[index] = str(prices[index]).replace('\xa0', '')
+    #         index += 1
+    #     info = item.xpath(".//span[contains(@class, 's-item__hotness')]/span/text()")
+    #     item_dict['name'] = names
+    #     item_dict['href'] = href
+    #     item_dict['price'] = prices[0]
+    #     try:
+    #         item_dict['info'] = info[0].replace('\xa0', '')
+    #     except IndexError:
+    #         item_dict['info'] = ''
+    #
+    #     items_list.append(item_dict)
+    #
+    # pprint(items_list)
