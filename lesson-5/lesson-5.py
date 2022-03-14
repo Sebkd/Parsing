e778414b98d05b3b6e7874236992654e9dc2ab21
Andrey_Serokhvostov <sebkd@mail.ru>
2022-03-14 22:59:40 +0300

    add webdriver

--- lesson-5/lesson-5.py
+++ lesson-5/lesson-5.py
@@ -1,1 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+driver.get('https://gb.ru/login')
