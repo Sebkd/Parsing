38e17ae966ff2b360095a8ab23327a0d5069dd9c
Andrey_Serokhvostov <sebkd@mail.ru>
2022-03-15 20:55:43 +0300

    add secret.py for login password

--- lesson-5/lesson-5.py
+++ lesson-5/lesson-5.py
@@ -1,7 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+from secret import LOGIN, PASSWORD
 
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
+# driver = webdriver.Chrome(executable_path='./chromedriver')
 driver.get('https://gb.ru/login')
+element = driver.find_element(By.ID, 'user_email')
+element.send_keys(LOGIN)
+element.send_keys(PASSWORD)
+while True:
+    pass
