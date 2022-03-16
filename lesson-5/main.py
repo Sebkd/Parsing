424e0e667c7c38281a340586cedb455c7870fb49
Andrey_Serokhvostov <sebkd@mail.ru>
2022-03-16 22:59:42 +0300

    break learning

--- lesson-5/main.py
+++ lesson-5/main.py
@@ -2,15 +2,31 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from secret import LOGIN, PASSWORD
 
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 # driver = webdriver.Chrome(executable_path='./chromedriver')
+
 driver.get('https://gb.ru/login')
+
 element = driver.find_element(By.ID, 'user_email')
 element.send_keys(LOGIN)
+
+element = driver.find_element(By.ID, 'user_password')
 element.send_keys(PASSWORD)
-while True:
-    pass
+
+element.send_keys(Keys.ENTER) # имитация нажатия клавиши
+
+input('continue')
+# print()
+#
+#
+# while True:
+#
+#
+#     pass
+#
+driver.close()
