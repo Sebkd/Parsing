816b41865632f0c03d195b11c40a8e4bf46c83fe
Andrey_Serokhvostov <sebkd@mail.ru>
2022-03-18 22:23:15 +0300

    let homework

--- lesson-5/picabu.py
+++ lesson-5/picabu.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
+import time
 
 service = Service('./chromedriver')
 
@@ -12,6 +13,13 @@ ua='cat'
 chrome_options.add_argument('--user-agent=%s' % ua)
 
 driver = webdriver.Chrome(service=service, options=chrome_options)
-driver.implicitly_wait(10)
+# driver.implicitly_wait(10)
 
 driver.get('https://pikabu.ru/')
+
+for i in range(5):
+    articles = driver.find_elements(By.TAG_NAME, 'article')
+    actions = ActionChains(driver) #накапливает события как стек
+    actions.move_to_element(articles[-1])
+    actions.perform()
+    time.sleep(4)
